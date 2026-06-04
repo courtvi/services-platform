caf431f0e37f2ec5c297c8432febbe3f3558fa98
courtvi <vincent.courtois0@gmail.com>
2026-06-04 11:06:50 +0200

    ovh deployment front end

---
+++ frontend/src/app/environments/environment.ovh.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://57.128.84.83.nip.io',
+  keycloak: {
+    url: 'http://57.128.84.83.nip.io',
+    realm: 'camping-haller',
+    clientId: 'haller'
+  },
+  paypal: {
+    clientId: 'AbVtQq3_8LphF3SZ8TRV4wX7s-lTLfySfkHDhmvxknXULaSAVBgfzUptTH2AAPRo4BxIkHuCZUjCUwZh'
+  }
+};
