203807e21b1900b6cc825fa31260b632d1db9f85
courtvi <vincent.courtois0@gmail.com>
2026-06-20 01:56:13 +0200

    ci/cd Jenkins +fix Maintainability

--- commande-service/src/main/java/org/gestion/commande/config/ApplicationConfig.java
+++ commande-service/src/main/java/org/gestion/commande/config/ApplicationConfig.java
@@ -1,10 +1,23 @@
 package org.gestion.commande.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @Configuration
 @EnableR2dbcRepositories
+@EnableConfigurationProperties(ApplicationConfig.RouteProperties.class)
 public class ApplicationConfig {
 
+    @ConfigurationProperties(prefix = "app.routes")
+    public static class RouteProperties {
+        private String basePath;
+        private String pathId;
+
+        public String getBasePath() { return basePath; }
+        public void setBasePath(String basePath) { this.basePath = basePath; }
+        public String getPathId() { return pathId; }
+        public void setPathId(String pathId) { this.pathId = pathId; }
+    }
 }
--- commande-service/src/main/java/org/gestion/commande/router/CommandeRouter.java
+++ commande-service/src/main/java/org/gestion/commande/router/CommandeRouter.java
@@ -1,5 +1,6 @@
 package org.gestion.commande.router;
 
+import org.gestion.commande.config.ApplicationConfig;
 import org.gestion.commande.handler.CommandeHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,19 +14,24 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 @Configuration
 public class CommandeRouter {
 
-    private static final String PATH_ID = "/{id}";
+    private final ApplicationConfig.RouteProperties routeProperties;
+
+    public CommandeRouter(ApplicationConfig.RouteProperties routeProperties) {
+        this.routeProperties = routeProperties;
+    }
 
     @Bean
     public RouterFunction<ServerResponse> commandeRoutes(CommandeHandler handler) {
+        String pathId = routeProperties.getPathId();
         return RouterFunctions.route()
                 .nest(path("/api/commandes"), builder -> builder
                         .POST("",        handler::createCommande)
                         .GET("",         handler::getCommandes)
-                        .GET(PATH_ID,    handler::getCommandeById)
-                        .GET("/{id}/detail",   handler::getCommandeAvecLignes)
-                        .PUT(PATH_ID,    handler::updateCommande)
-                        .DELETE(PATH_ID, handler::annulerCommande)
-                        .PATCH("/{id}/en-cours", handler::passerEnCours)
+                        .GET(pathId,    handler::getCommandeById)
+                        .GET(pathId + "/detail",   handler::getCommandeAvecLignes)
+                        .PUT(pathId,    handler::updateCommande)
+                        .DELETE(pathId, handler::annulerCommande)
+                        .PATCH(pathId + "/en-cours", handler::passerEnCours)
                         .GET("/debug", request ->
                                 ReactiveSecurityContextHolder.getContext()
                                         .map(ctx -> ctx.getAuthentication())
@@ -33,7 +39,6 @@ public class CommandeRouter {
                                                 ServerResponse.ok().bodyValue(auth)
                                         )
                         )
-
                 )
                 .build();
     }
