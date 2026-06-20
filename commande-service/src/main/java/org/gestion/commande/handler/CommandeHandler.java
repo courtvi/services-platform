78e8bfc17390da25675c435137aab4e158fdcba7
courtvi <vincent.courtois0@gmail.com>
2026-06-20 02:49:51 +0200

    ci/cd Jenkins +fix Maintainability

--- commande-service/src/main/java/org/gestion/commande/handler/CommandeHandler.java
+++ commande-service/src/main/java/org/gestion/commande/handler/CommandeHandler.java
@@ -68,11 +68,7 @@ public class CommandeHandler {
                 })
                 .flatMap(response ->
                         ServerResponse.status(201).bodyValue(response)
-                )
-                .onErrorResume(e -> {
-                    e.printStackTrace();
-                    return ServerResponse.badRequest().bodyValue(e.getMessage());
-                });
+                );
     }
 
     public Mono<ServerResponse> getCommandes(ServerRequest request) {
